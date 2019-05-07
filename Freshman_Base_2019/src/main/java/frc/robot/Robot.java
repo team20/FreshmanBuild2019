ebd68fee665cec8c4016ea07e07fe2098e6b23fe
20LM22 <etoile22lm@gmail.com>
2019-05-07 19:01:51 -0400

    Fun autos but no line sensor

--- Freshman_Base_2019/src/main/java/frc/robot/Robot.java
+++ Freshman_Base_2019/src/main/java/frc/robot/Robot.java
@@ -7,7 +7,8 @@ import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.TimedRobot;
-//import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Timer;
+import java.lang.Math;
 
 public class Robot extends TimedRobot {
     static Joystick driverJoy;
@@ -29,14 +30,22 @@ public class Robot extends TimedRobot {
     static Pneumatics maticsMatics = new Pneumatics();
     static LineFollower lineFollower = new LineFollower();
     static RealAutos autos = new RealAutos();
+    static AutoCompilation tryAutosAgain = new AutoCompilation();
 
     boolean startAuto = false;
     boolean startAutoSet = false;
 
+    boolean startTimeSet = false;
+    boolean startTimeSetTwo = false;
+    static double startTime = 0;
+    static double goNow = 0;
+
+    double pi = Math.PI;
+
     @Override
     public void robotInit() {
-        driverJoy = new Joystick(0);
-        operatorJoy = new Joystick(1);
+        driverJoy = new Joystick(1);
+        operatorJoy = new Joystick(0);
         CameraServer.getInstance().startAutomaticCapture();
         NAVXgyro.reset();
         drivingClass.initQuadrature();
@@ -45,32 +54,32 @@ public class Robot extends TimedRobot {
     @Override
     public void autonomousInit() {
 
-        NAVXgyro.reset();
-        firstTime = true;
+        if (startTimeSet == false) {
+            startTime = Timer.getFPGATimestamp();
+            startTimeSet = true;
+        }
 
+        if (startTimeSetTwo == false) {
+            goNow = Timer.getFPGATimestamp();
+            startTimeSetTwo = true;
+        }
     }
 
     @Override
     public void autonomousPeriodic() {
+        if (!drivingClass.moveDistance(60, .25)) {
+            drivingClass.moveDistance(60, .25);
+        } else if (drivingClass.moveDistance(60, .25)) {
+            shootNow.autoShoot();
+        }
 
-        // if (startAuto == false && autos.firstAuto(true, false, false, 1, 0, 60, .25, 60)) {
-        //     startAuto = true;
-        // }
-        // spinningIntake.spinIntake(true, false, true);
-        //
-        // shootNow.operatorShoot(operatorJoy.getRawAxis(1),
-        // operatorJoy.getRawAxis(5)/2);
-        // if (startAuto == false && drivingClass.moveDistance(24.0, .25) == true) {
-        // startAuto = true;
-        // }
-        // drivingClass.turnAngle(60);
-
+        //drivingClass.spin((22.5 * pi), .25);
     }
 
     @Override
     public void teleopPeriodic() {
 
-        drivingClass.move(driverJoy.getRawAxis(1)*.3, driverJoy.getRawAxis(2)*.3, driverJoy.getRawAxis(3)*.3);
+        drivingClass.move(driverJoy.getRawAxis(1) * .3, driverJoy.getRawAxis(2) * .3, driverJoy.getRawAxis(3) * .3);
         spinningIntake.spinIntake(driverJoy.getRawButton(1), driverJoy.getRawButton(4), driverJoy.getRawButton(2));
         shootNow.operatorShoot(operatorJoy.getRawAxis(1), operatorJoy.getRawAxis(5));
         maticsMatics.openGrabbyClaw(operatorJoy.getRawAxis(2));
@@ -86,3 +95,5 @@ public class Robot extends TimedRobot {
     public void testPeriodic() {
     }
 }
+
+// Need to put LED code into the regular code
--- Freshman_Base_2019/src/main/java/frc/robot/Shooter.java
+++ Freshman_Base_2019/src/main/java/frc/robot/Shooter.java
@@ -3,6 +3,8 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.wpilibj.Timer;
+
 public class Shooter {
     VictorSPX shooter;
 
@@ -21,14 +23,29 @@ public class Shooter {
             shooter.set(ControlMode.PercentOutput, .6);
         }
         // } else if (Robot.driverJoy.getRawButton(1) == true) {
-        //     shooter.set(ControlMode.PercentOutput, -1);
+        // shooter.set(ControlMode.PercentOutput, -1);
         // } else if (Robot.driverJoy.getRawButton(4) == true) {
-        //     shooter.set(ControlMode.PercentOutput, -1);
+        // shooter.set(ControlMode.PercentOutput, -1);
         // } else if (Robot.driverJoy.getRawButton(2) == true) {
-        //     shooter.set(ControlMode.PercentOutput, 0);
+        // shooter.set(ControlMode.PercentOutput, 0);
         // }
         else {
             shooter.set(ControlMode.PercentOutput, 0);
         }
     }
+
+    public void autoShoot() {
+        if ((Timer.getFPGATimestamp() - Robot.startTime) <= 4) {
+            shooter.set(ControlMode.PercentOutput, -1);
+        }
+        else if ((Timer.getFPGATimestamp() - Robot.startTime) > 4) {
+            shooter.set(ControlMode.PercentOutput, 0);
+        }
+        else {
+        }
+    }
+
+    // public void autoShoot() {
+    // shooter.set(ControlMode.PercentOutput, -.5);
+    // }
 }
